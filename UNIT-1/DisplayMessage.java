9859631278488f926390ed2b4f0f1cd6134b9d43
Mayursinh D Jadeja <mayursinhdjadeja01@gmail.com>
2026-07-08 20:05:47 +0530

    Create DisplayMessage.java

---
+++ UNIT-1/DisplayMessage.java
@@ -0,0 +1,9 @@
+// Write a simple java program to display message.
+
+public class DisplayMessage
+{
+    public static void main(String argc[])
+    {
+        System.out.println("Hello , Wellcome to the world of java technology.");
+    }
+}
