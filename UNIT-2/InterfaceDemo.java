e1147470450768020290c96d95b3d321a7652270
Mayursinh D Jadeja <mayursinhdjadeja01@gmail.com>
2026-08-13 13:01:10 +0530

    Add files via upload

---
+++ UNIT-2/InterfaceDemo.java
@@ -0,0 +1,25 @@
+/*
+ * Write a java program to use Interface in java.
+*/
+
+interface Animal 
+{
+    void sound();
+}
+
+class Dog implements Animal 
+{
+    public void sound() 
+    {
+        System.out.println("Dog barks");
+    }
+}
+
+public class InterfaceDemo 
+{
+    public static void main(String args[]) 
+    {
+        Animal ob = new Dog();
+        ob.sound();
+    }
+}
---
+++ UNIT-2/InterfaceExtendDemo.java
@@ -0,0 +1,35 @@
+/*
+ * Write a java program to extend one interface into another interface.
+*/
+
+interface A 
+{
+    void methodA();
+}
+
+interface B extends A 
+{
+    void methodB();
+}
+
+class Demo implements B 
+{
+    public void methodA() 
+    { 
+        System.out.println("Method A"); 
+    }
+    public void methodB() 
+    { 
+        System.out.println("Method B"); 
+    }
+}
+
+public class InterfaceExtendDemo 
+{
+    public static void main(String args[]) 
+    {
+        Demo ob = new Demo();
+        ob.methodA();
+        ob.methodB();
+    }
+}
