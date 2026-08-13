5c2abb1bb438d1f55be736e428b29757879986e0
Mayursinh D Jadeja <mayursinhdjadeja01@gmail.com>
2026-08-13 12:45:37 +0530

    Add files via upload

---
+++ UNIT-2/MethodOverriding.java
@@ -0,0 +1,29 @@
+/*
+ * Write a java program to use method overriding.
+*/
+
+class Father 
+{
+    void display() 
+    { 
+        System.out.println("Father"); }
+}
+
+class Son extends Father 
+{
+    void display() 
+    { 
+        System.out.println("Son"); 
+    }
+}
+
+public class MethodOverriding 
+{
+    public static void main(String args[]) 
+    {
+        Father ob1 = new Son();
+        Father ob2 = new Father();
+        ob1.display();
+        ob2.display();
+    }
+}
